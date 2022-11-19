8740574c445dbf3cbc4cbe4630c59c4aafe99b2f
Prabudhd Krishna Kandpal <96969241+prabudhd2003@users.noreply.github.com>
2022-11-19 17:58:26 +0530

    Add files via upload

---
+++ crypto-website/src/App.js
@@ -0,0 +1,9 @@
+import CoinHome from "./CoinHome";
+
+export default function App(){
+    return(
+        <div>
+            <CoinHome />
+        </div>
+    )
+}
---
+++ customizing-example/src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo(){
+    return(
+        <div className = "promo-section">
+            <div>
+                <h1>Don't miss this deal!</h1>
+            </div>
+            <div>
+                <h2>Subcribe to the newsletter to get all the information!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
