12031ec316d8374fb64206abf322387b155cdeb7
Prabudhd Krishna Kandpal <96969241+prabudhd2003@users.noreply.github.com>
2024-08-31 19:11:30 +0530

    Delete functional-components directory

--- functional-components/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header(props){
-    console.log(props);
-    return(
-        <h1>Hello there indeed, {props.color},  
-        {props.name} </h1>
-    )
-};
-
-export default Header;
--- functional-components/src/components/Main.js
+++
@@ -1,7 +0,0 @@
-function Main(props){
-    return(
-        <h2>{props.greet} from Main</h2>
-    );
-};
-
-export default Main;
